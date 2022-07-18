5969ca6ba41bd1057747067e55c333cf35ff350e
gracjanch <gchorazak4@gmail.com>
2022-07-18 17:27:41 +0200

    first commit

---
+++ src/test/java/task/gitAPI/GitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package task.gitAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
