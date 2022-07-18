e1f0d74c619f1d43a582a0f0779487f92dfca363
gracjanch <gchorazak4@gmail.com>
2022-07-18 18:46:46 +0200

    added: connection with Git API and model DTO

---
+++ src/main/java/task/gitAPI/connection/GitApiConnection.java
@@ -0,0 +1,42 @@
+package task.gitAPI.connection;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import task.gitAPI.model.GitInfo;
+import task.gitAPI.model.GitInfoDto;
+
+@Component
+@RequiredArgsConstructor
+public class GitApiConnection {
+
+    private static final String GIT_API_URL = "https://api.github.com/users/";
+    private RestTemplate restTemplate = new RestTemplate();
+
+    public GitInfo getInfoForUserAndRepoName(String owner, String repositoryName) {
+
+        GitInfoDto[] repositories = callGitApi(owner);
+        for (int i = 0; i < repositories.length; i++) {
+            GitInfoDto gitInfoDto = repositories[i];
+            String repName = gitInfoDto.getName().toLowerCase();
+
+            if (repName.equals(repositoryName.toLowerCase())) {
+                return GitInfo.builder()
+                        .fullName(gitInfoDto.getFull_name())
+                        .description(gitInfoDto.getDescription())
+                        .cloneUrl(gitInfoDto.getClone_url())
+                        .stars(gitInfoDto.getStargazers_count())
+                        .createdAt(gitInfoDto.getCreated_at())
+                        .build();
+            }
+        }
+        return null;
+    }
+
+    private GitInfoDto[] callGitApi(String owner) {
+        return restTemplate.getForObject(
+                GIT_API_URL + "{owner}/repos",
+                GitInfoDto[].class,
+                owner);
+    }
+}
---
+++ src/main/java/task/gitAPI/model/GitInfoDto.java
@@ -0,0 +1,18 @@
+package task.gitAPI.model;
+
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@Getter
+public class GitInfoDto {
+
+    private String name;
+    private String full_name;
+    private String description;
+    private String clone_url;
+    private Integer stargazers_count;
+    private LocalDateTime created_at;
+}
